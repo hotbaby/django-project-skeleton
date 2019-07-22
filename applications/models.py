7706701654b357cd7b1d31822fd6193172a1c03d
yy <mengyy_linux@163.com>
2019-07-22 16:01:36 +0800

    cleanup code

--- applications/models.py
+++ applications/models.py
@@ -10,4 +10,3 @@ class BaseModel(models.Model):
     state = models.IntegerField(default=1, help_text='物理状态')
     create_time = models.DateTimeField(auto_now_add=True, help_text='创建时间')
     update_time = models.DateTimeField(auto_now=True, help_text='更新时间')
-
